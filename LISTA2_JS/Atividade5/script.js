0c991832920b74aba870e351c1cfe532796d5531
Savio <78896606+SavioMariano@users.noreply.github.com>
2024-05-12 19:05:57 -0300

    Update script.js

--- LISTA2_JS/Atividade5/script.js
+++ LISTA2_JS/Atividade5/script.js
@@ -3,7 +3,8 @@ function vowels() {
 
     if (isNaN(vowels)) {
         var onlyVowels = vowels.replaceAll(/[^áéíóúaeiouAEIOU]/g, "");
-        console.log("Na palavra '" + vowels + "' existem essas vogais: " + onlyVowels);
+        console.log("Na palavra '" + vowels + "' existem essas vogais: " + onlyVowels); // Printa as vogais
+        console.log("Existem " + onlyVowels.length + " de vogais"); //Printa a quantidade de vogais.
     } else {
         console.log("Escreva uma palavra.");
     }
