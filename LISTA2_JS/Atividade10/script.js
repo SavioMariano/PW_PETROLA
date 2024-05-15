5ef2719b108592c59c297f09ac3a0aa2d42b0de5
Savio <78896606+SavioMariano@users.noreply.github.com>
2024-05-15 20:25:50 -0300

    Update script.js

--- LISTA2_JS/Atividade10/script.js
+++ LISTA2_JS/Atividade10/script.js
@@ -1,12 +1,8 @@
     function len(){
-        var phrase = "";
-        var phrases = window.prompt("Digite uma frase: ");
-
-        for(i = 0; i <= phrases.length - 1; i++ ){
-            if (phrases[i] !== " "){
-            phrase += phrases[i];
-            }
-        }
-        console.log(phrase.length);
+        var phrase = window.prompt("Digite uma frase: ");
+        
+        console.log("A frase '" + phrase + "' tem " + phrase.split(' ').length + " palavra(s)");
+        //.split('') aspas simples juntas conta as caracteres.
+        //.split(' ') aspas simples separada conta as palavras.
     }
 len();
