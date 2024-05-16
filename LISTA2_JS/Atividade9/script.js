92e05740e25f956623fae740cce2a07cbce7e20d
Savio <78896606+SavioMariano@users.noreply.github.com>
2024-05-16 15:54:02 -0300

    Update script.js

--- LISTA2_JS/Atividade9/script.js
+++ LISTA2_JS/Atividade9/script.js
@@ -1,18 +1,31 @@
-document.getElementById('inverter').addEventListener('submit', function(event){
+document.getElementById('inverter').addEventListener('submit', function (event) {
     event.preventDefault();
 
     var reverse = document.getElementById("words-reverse");
     var word = document.getElementById("words").value;
-    var value = "";
+    var values = "";
+    var ward = "";
 
-    for(var i = word.length - 1; i >= 0; i-- ){
-        value += word[i];
+    for (var i = 0; i <= word.length - 1; i++) {
+        if (word[i] !== " ") {
+            ward += word[i];
+        }
     }
-        reverse.value = value;
 
-    if (reverse.value === word){
+    for (var i = word.length - 1; i >= 0; i--) {
+        if (word[i] !== " ") {
+            values += word[i];
+        }
+    }
+
+    console.log(ward.replaceAll(/[.:;,]/g, ''));
+    console.log(values.replaceAll(/[.:;,]/g, ''));
+
+    reverse.value = values;
+
+    if (values.toLowerCase().replaceAll(/[.:;,]/g, '') === ward.toLowerCase().replaceAll(/[.:;,]/g, '')) {
         console.log(true);
-    }else{
+    } else {
         console.log(false);
     }
 
